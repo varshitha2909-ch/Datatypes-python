f393100ecacb9465dcf6a58b74285b2e0340727d
varshitha2909-ch <chennareddyvarshitha@gmail.com>
2026-08-10 11:36:07 +0530

    Add list methods for numbers and fruits
    Implement various list methods for numbers and fruits.

---
+++ list.py
@@ -0,0 +1,28 @@
+#####List Methods#####
+numbers=[10,200,30,40]
+print(numbers)
+numbers.append(50)
+print(numbers)
+numbers.insert(3,70)
+print(numbers)
+numbers.remove(50)
+print(numbers)
+numbers.pop()
+print(numbers)
+numbers.sort()
+print(numbers)
+numbers.reverse()
+print(numbers)
+
+#################
+fruits=['apple','mango','banana','orange','guava']
+print(fruits)
+print(type(fruits))
+
+print(len(fruits))
+
+print(fruits[1:4])
+print(fruits[::-1])
+
+for i in fruits:
+    print(i)
