585924a610de0b269130990c4a2bff4f6db4d9e0
varshitha2909-ch <chennareddyvarshitha@gmail.com>
2026-08-10 11:23:15 +0530

    Add tuple examples and basic operations

---
+++ tuple.py
@@ -0,0 +1,13 @@
+########
+T1=(9,1,7,"Python",6,8,"Java",4,"HTML",7,6,2,1)
+print(T1)
+print(type(T1))
+
+
+#########
+T1=(9,1,7,6,8,7,4,7,6,2,1,0,5)
+print(T1)
+print(type(T1))
+print(max(T1))
+print(min(T1))
+print(len(T1))
